20ff9c8e206d506b11da85e2f08937af7a5c345b
luanxh <18734184215@163.com>
2022-04-13 15:18:17 +0800

    学习C语言的第一课

---
+++ cmake-build-debug/CMakeFiles/c_project_one.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_project_one.dir/main.c.o"
+  "c_project_one"
+  "c_project_one.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_project_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
